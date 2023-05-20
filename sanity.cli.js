08ade4f693dabc2c2a90cda6ba2b1f9000d7bc2d
Ankit Sharma <sharmaa7861@gmail.com>
2023-05-20 23:42:29 +0530

    updating changes

--- sanity.cli.js
+++ sanity.cli.js
@@ -5,7 +5,7 @@
 import { defineCliConfig } from 'sanity/cli'
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
+const dataset = process.env.EXT_PUBLIC_SANITY_DATASET
 
 export default defineCliConfig({ api: { projectId, dataset } })
  
