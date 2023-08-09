86bfd7f8144590b2f03345e785b973e0bb1f8597
Ankit Sharma <sharmaa7861@gmail.com>
2023-08-09 19:50:29 +0545

    completed: auth route

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const errorMiddleWare = require("./middleware/error");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 app.use(express.json());
+
+app.use(cookieParser());
 
 //route imports
 const product = require("./routes/productRoute");
 const user = require("./routes/userRoute");
 
 app.use("/api/v1", product);
-app.use("/api/v1", user); 
+app.use("/api/v1", user);
 
 app.use(errorMiddleWare);
 
