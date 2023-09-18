ef2d5fe9383e9e48e9af274067412b6601c50d9a
Ankit Sharma <sharmaa7861@gmail.com>
2023-09-18 10:56:23 +0545

    Price Filter added

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -22,14 +22,19 @@ exports.getAllProducts = catchAsyncError(async (req, res, next) => {
 
   const apiFeature = new ApiFeatures(Product.find(), req.query)
     .search()
-    .filter()
-    .pagination(resultPerPage);
-  const products = await apiFeature.query;
+    .filter();
+
+  let products = await apiFeature.query;
+  let filteredProductsCount = products.length;
+  apiFeature.pagination(resultPerPage);
+
+  products = await apiFeature.query;
   res.status(200).json({
     success: true,
     products,
     productsCount,
     resultPerPage,
+    filteredProductsCount
   });
 });
 
--- electronica/src/component/Product/Products.js
+++ electronica/src/component/Product/Products.js
@@ -5,27 +5,38 @@ import { clearErrors, getProduct } from "../../actions/productActions";
 import ProductCard from "../layout/home/ProductCard";
 import Loader from "../layout/loader/Loader";
 import Pagination from "react-js-pagination";
+import Slider from "@material-ui/core/Slider";
+import Typography from "@material-ui/core/Typography";
 
 const Products = ({ match }) => {
   const dispatch = useDispatch();
-  const { products, loading, error, productsCount, resultPerPage } =
-    useSelector((state) => state.products);
+  const {
+    products,
+    loading,
+    error,
+    productsCount,
+    resultPerPage,
+    filteredProductsCount,
+  } = useSelector((state) => state.products);
 
   const [currentPage, setCurrentPage] = useState(1);
+  const [price, setPrice] = useState([0, 55000]);
 
   const keyword = match.params.keyword;
   const setCurrentPageNo = (e) => {
     setCurrentPage(e);
   };
-  console.log(
-    "resultPerPage: ",
-    resultPerPage,
-    "ProductsCount: ",
-    productsCount
-  );
+
+  const priceHandler = (event, newPrice) => {
+    setPrice(newPrice);
+  };
+
   useEffect(() => {
-    dispatch(getProduct(keyword, currentPage));
-  }, [dispatch, keyword, currentPage]);
+    dispatch(getProduct(keyword, currentPage, price));
+  }, [dispatch, keyword, currentPage, price]);
+
+  let count = filteredProductsCount;
+
   return (
     <Fragment>
       {loading ? (
@@ -39,7 +50,20 @@ const Products = ({ match }) => {
                 <ProductCard key={product._id} product={product} />
               ))}
           </div>
-          {resultPerPage < productsCount && (
+
+          <div className="filterBox">
+            <Typography>Price</Typography>
+            <Slider
+              value={price}
+              onChange={priceHandler}
+              valueLabelDisplay="auto"
+              aria-labelledby="range-slide"
+              min={0}
+              max={55000}
+            />
+          </div>
+
+          {resultPerPage < count && (
             <div className="paginationBox">
               <Pagination
                 activePage={currentPage}
