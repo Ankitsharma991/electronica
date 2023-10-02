e135b88cdf0967319ac59495c380574e66151d71
Ankit Sharma <sharmaa7861@gmail.com>
2023-10-02 11:05:51 +0545

    adding authentication

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const errorMiddleWare = require("./middleware/error");
 const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const fileUpload = require("express-fileupload");
 
 const app = express();
 app.use(express.json());
-
 app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(fileUpload);
 
 //route imports
 const product = require("./routes/productRoute");
